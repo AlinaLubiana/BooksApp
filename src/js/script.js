8777acac7a13227c93cdf3178b6859bd25fca339
Alina  Lubiana <alina.furman28@gmail.com>
2023-01-05 21:41:06 +0200

    add9.2 ex2

--- src/js/script.js
+++ src/js/script.js
@@ -13,14 +13,38 @@ const templates = {
     bookTemplate: Handlebars.compile(document.querySelector(select.templateOf.bookTemplate).innerHTML),
 };
 
+let favoriteBooks = [];
+
 function renderBooks(){
 
     for(let bookData in dataSource.books){
         const generatedHTML = templates.bookTemplate(dataSource.books[bookData]);
         let bookElement = utils.createDOMFromHTML(generatedHTML);
-
         const bookContainer = document.querySelector(select.containerOf.bookList);
         bookContainer.appendChild(bookElement);
     }
 }
+
+function initActions() {
+
+    const bookContainer = document.querySelectorAll('a.book__image');
+
+    for (let i = 0; i < bookContainer.length; i++){
+        bookContainer[i].addEventListener('dblclick', function(event){
+            event.preventDefault();
+            bookContainer[i].classList.add('favorite');
+            let bookID = bookContainer[i].getAttribute('data-id');
+
+            if (!favoriteBooks.includes(bookID)){
+                favoriteBooks.push(bookID);
+            }
+            // console.log('obj', bookContainer[i].getAttribute('data-id'));
+            // console.log('favoriteBooks', favoriteBooks);
+        });
+    }
+}
+
 renderBooks();
+initActions();
+
+
